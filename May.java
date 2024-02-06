2e9232f7043a96455f5aac52c33440c60b267cab
may772 <122054558+may772@users.noreply.github.com>
2024-02-06 20:23:13 +0200

    Update May.java

--- May.java
+++ May.java
@@ -2,6 +2,7 @@ public class May {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
             System.out.println("May Shviro");
+            System.out.println("New Print");
         }
     }
 }
